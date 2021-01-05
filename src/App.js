0436500a0e933fca5dbef8b9adbb9f409b64af2c
wallacemachado <wallacefm18@gmail.com>
2021-01-05 14:35:08 -0300

    Props e component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+const Bemvindo = (props) => {
+return (<div> 
+<h2>Bem-vindo {props.nome}</h2>
+<h2>Tenho: {props.idade}</h2>
+</div>)
+
+}
+
 //componente principal - cria  pagina principal
 function App() {
   return(
-
+    <div>
+      Olá Mundo!
+      <Bemvindo nome="Lucas" idade="24"/>
+      <Bemvindo nome="sara" idade="21"/>
+      <Bemvindo nome="leo" idade="18"/>
    <h1>Olá mundo</h1>
+   </div>
   )
 }
 

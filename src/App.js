d85321bcfcd3827e51f15f6239efce3dc594b668
wallacemachado <wallacefm18@gmail.com>
2021-01-05 17:23:00 -0300

    ciclo de vida dos componentes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
 import React, {Component} from 'react';
 
+//sobre os clicos de vidas dos componentes
 class App extends Component{
 
-    constructor(props){ //construtor da classe e recebe as props
-      super(props)// para usar as props do classe pai
-      this.state = { // informa todos os componentes que fazem parte da classe
-        nome: "Matheus",
-        contador: 0
+    constructor(props){ //construtor é o primeiro a ser carregado
+      super(props)
+      this.state = { 
+        hora: '00:00:00'
       };
-
-      this.aumentar = this.aumentar.bind(this);// para functions poderem ser usadas
-      this.diminuir = this.diminuir.bind(this);
+      
     }
+    //componentDidMount é o segundo a ser carregado é só é executado uma vez quando a pagia carrega
+    componentDidMount(){
+        setInterval(()=>{this.setState({hora: new Date().toLocaleTimeString()})
 
-    aumentar(){
-      let state = this.state;// recebe tudo que está dentro de state
-     state.contador += 1;
-      this.setState(state)
+        },1000); //fica exucutando algo apos de intevalo de tempo, nesse é um segundo
     }
-
-    diminuir(){
-      let state = this.state;// recebe tudo que está dentro de state
-      if(state.contador === 0){
-        alert('opa chegou a zero');
-        return;
-      }
-      state.contador -= 1;
-      this.setState(state)
-    }
+   
+    // é executado sempre que o componente state é atualizado
+      componentDidUpdate(){
+        console.log('atualizou');
+        }
+        
+      
       render(){//método obrigatório
         return(
 
           <div> 
-            <h1>Contador</h1>
-            {this.state.nome}
-            <h3>
-            <button onClick={this.diminuir}>-</button>
-            {this.state.contador}
-            <button onClick={this.aumentar}>+</button>
-            </h3>
+            <h1>{this.state.hora}</h1>
+            
           </div>
         );
       }
@@ -48,42 +38,56 @@ class App extends Component{
 
 export default App;
 
-/*Vamos lá relembrar quais são os Tipos de Componentes?
+/*Ciclos de vida dos Componentes
+Então como vimos na aula nos ciclos de vida do componente temos:
 
-Como você viu nas aulas no ReactJS nós temos 2 principais tipos de componentes: Componentes com Estados e Componentes sem Estados
+Inicialização
 
-Também são conhecidos por (StateLess e StateFull);
+Montagem
 
-Os dois componentes tem as mesmas características, já citadas anteriomente, com a diferença que: um componente com estados possui atributos que poderão ter o seu valor alterado, e a cada alteração, impactarão diretamente na renderização do componente.
+Atualização
 
-Já, os componentes sem estados, são componentes mais simples, que quase sempre só servem para renderizar uma responsabilidade pequena de nossa página.
+Desmontagem
 
-Outra diferença muito importante é a forma como estes componentes são escritos.
 
-Um componente com estados quase sempre vai ser representado, de fato, por uma classe, por exemplo:
 
-import React, { Component } from "react";
- 
-class App extends Component {
-  render() {
-    return(
-    <h1>Olá ReactJS</h1>
-    );
-  }
-}
- 
-export default App;
-Note que para este componente nós temos obrigatoriamente que utilizar o método render() pois ele será o responsável por colocar e recolocar as informações na tela.
+- Inicialização
 
-Mas, EXATAMENTE o mesmo componente, poderia ser escrito da seguinte forma:
+Nesta fase, o componente React se prepara para sua inicialização, configurando os estados iniciais e props padrões se houverem.
 
-import React from "react";
- 
-const App = () => {
-  return( 
-    <h1>Olá ReactJS</h1>
-  );
-};
- 
-export default App;
+
+
+- Montagem
+
+Depois de preparar com todas as necessidades básicas, estado e props, o nosso Componente React está pronto para ser montado no DOM do navegador.
+
+componentWillMount() É executado quando o componente estiver prestes a ser montado no DOM da página. Assim, após esse método ser executado o componente irá criar o nó no navegador. Todas as coisas que você deseja fazer antes do componente ser montado, devem ser definidas aqui.
+
+
+
+componentDidMount() Este é o método que é executado depois que o componente foi montado no DOM
+
+
+
+- Atualização
+
+Esta fase começa quando o componente React já nasceu no navegador e cresce recebendo novas atualizações. O componente pode ser atualizado de duas maneiras, através do envio de novas props ou a atualização do seu estado.
+
+
+
+componentDidUpdate() é chamado imediatamente após a atualização.
+
+
+
+componentWillUpdate() É executado somente quando shouldComponentUpdatedevolver true.
+
+
+
+- Desmontagem
+
+Nesta fase, o componente não é mais necessário e será desmontado do DOM. O método que se chama nesta fase é o:
+
+componentWillUnmount()
 */
+
+

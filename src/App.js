9f074f07264260ed1b97be7d5f06eec7ae8e372b
wallacemachado <wallacefm18@gmail.com>
2021-01-05 15:37:11 -0300

    trabalhando com state

--- src/App.js
+++ src/App.js
@@ -1,61 +1,49 @@
 import React, {Component} from 'react';
 
-class Equipe extends Component{
+class App extends Component{
+
+    constructor(props){ //construtor da classe e recebe as props
+      super(props)// para usar as props do classe pai
+      this.state = { // informa todos os componentes que fazem parte da classe
+        nome: "Matheus",
+        contador: 0
+      };
+
+      this.aumentar = this.aumentar.bind(this);// para functions poderem ser usadas
+      this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+      let state = this.state;// recebe tudo que está dentro de state
+     state.contador += 1;
+      this.setState(state)
+    }
+
+    diminuir(){
+      let state = this.state;// recebe tudo que está dentro de state
+      if(state.contador === 0){
+        alert('opa chegou a zero');
+        return;
+      }
+      state.contador -= 1;
+      this.setState(state)
+    }
       render(){//método obrigatório
         return(
 
           <div> 
-            <Sobre nome={this.props.nome} idade={this.props.cargo} cargo={this.props.idade}/>
-
-            <Social/>
-         <hr/>
+            <h1>Contador</h1>
+            {this.state.nome}
+            <h3>
+            <button onClick={this.diminuir}>-</button>
+            {this.state.contador}
+            <button onClick={this.aumentar}>+</button>
+            </h3>
           </div>
         );
       }
 
 }
 
-class Sobre extends Component{
-
-  render(){//método obrigatório
-    return(
-
-      <div> 
-        
-            <h2>Bem-vindo {this.props.nome}</h2>
-            <h3>Sou: {this.props.cargo}</h3>
-            <h3>Tenho: {this.props.idade}anos </h3>
-          
-        
-      </div>
-    );
-  }
-
-}
-
-const Social =() =>{
-  return(
-  <div> 
-        
-            <a>Facebook </a>
-            <a>Instagram </a>
-            <a>LinkedIn  </a>
-          
-        
-      </div>
-  );
-}
-
-//componente principal - cria  pagina principal
-function App() {
-  return(
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe nome="Lucas" idade="24" cargo="programador"/>
-     
-   
-   </div>
-  );
-}
 
 export default App;

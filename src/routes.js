3a219de12edc29e640da7dd1a408b0ac9afd5e3c
wallacemachado <wallacefm18@gmail.com>
2021-01-06 23:25:13 -0300

    rotas de paginas parte 2

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'; //necessário instalar npm install react-router-dom
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
+import Erro from './pages/Erro';
+import Produto from './pages/Produto';
+
+
 import Header from './components/Header';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Header/>
-            <Switch>{/*garante que só será carregada uma rota por vez */}
+            <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/sobre" component={Sobre} />
+                <Route path="/produto/:id" component={Produto}/>
+                <Route path="*" component={Erro /* tem que estar apos todas as outras paginas, caso solicite uma pagina que não existe */}/>
             </Switch>
         </BrowserRouter>
     );
